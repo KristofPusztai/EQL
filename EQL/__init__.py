ca687185e20989366af88d69e27c09f318ea3a50
Kristof Pusztai <kpusztai@berkeley.edu>
2020-11-26 16:34:03 -0500

    setup

---
+++ EQL/__init__.py
@@ -0,0 +1,2 @@
+# Version of the EQL package
+__version__ = "1.0.0"
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Kristof Pusztai",
+    author_email="kpusztai@berkeley.edu",
+    description="An implementation of Equation Learning Based Neural Network Model",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

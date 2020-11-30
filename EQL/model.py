f647477f006d31c96b3ca50bfff1b9adc07597b8
Kristof Pusztai <kpusztai@berkeley.edu>
2020-11-30 16:24:43 -0500

    update 1

--- EQL/model.py
+++ EQL/model.py
@@ -1,2 +1,38 @@
-import numpy as np
+import tensorflow as tf
 
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class EQL:
+    def __init__(self, num_layers=2):
+        self.num_layers = num_layers
+        self.model = None
+
+    def __build_and_compile_model(self):
+        total_layers = []
+        for i in range(self.num_layers):
+            id_out = tf.keras.layers.Dense(units=1, activation=tf.identity)
+            sin_out = tf.keras.layers.Dense(units=1, activation=tf.math.sin)
+            cos_out = tf.keras.layers.Dense(units=1, activation=tf.math.cos)
+            sigmoid_out = tf.keras.layers.Dense(units=1, activation=tf.nn.sigmoid)
+
+            out = tf.concat([id_out, sin_out, cos_out, sigmoid_out...], axis=1)
+            # TODO: Implement custom layer
+            total_layers.append(tf.keras.layers.Dense(6, activation=tf.math.sin))
+        total_layers.append(layers.Dense(1))
+        model = keras.Sequential(total_layers)
+
+        model.compile(loss='mean_absolute_error', optimizer=tf.keras.optimizers.Adam(0.001))
+        return model
+
+    def fit(self, x, y):
+        self.model = self.__build_and_compile_model()
+        self.model.fit(x, y, epochs=100,
+                       # suppress logging
+                       verbose=0,
+                       # Calculate validation results on 20% of the training data
+                       validation_split=0.2)
+
+    def predict(self, x):
+        return self.model.predict(x)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="EQL", # Replace with your own username
-    version="0.0.4",
+    version="0.0.5",
     author="Kristof Pusztai",
     author_email="kristofp12@gmail.com",
     description="An implementation of Equation Learning Based Neural Network Model",
@@ -14,7 +14,6 @@ setuptools.setup(
     url="https://github.com/KristofPusztai/EQL",
     packages=setuptools.find_packages(),
     install_requires=[
-        'numpy',
         'tensorflow',
     ],
     classifiers=[
